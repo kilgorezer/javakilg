fe84dcacc7daacdb298ca53cbe0ad914972eadc5
kilgorezer <74324783+kilgorezer@users.noreply.github.com>
2024-08-29 20:22:54 -0700

    Update A1.js

--- releases/A1.js
+++ releases/A1.js
@@ -37,6 +37,7 @@ JavaKilg.new = function(obj) {
   _.call3D = function(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, _a, _b) {
     return this.contents[a][b][c](d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, _a, _b);
   };
+  _.constructor = JavaKilg.new;
   return _;
 };
 JavaKilg.Arr2D = function(x, y) {
