6fe01b7c80ccb63d18a58c28144d7578fb9dd2e1
kilgorezer <74324783+kilgorezer@users.noreply.github.com>
2024-08-29 19:46:21 -0700

    Update A1.js

--- releases/A1.js
+++ releases/A1.js
@@ -9,5 +9,53 @@ JavaKilg.new = function(obj) {
   _.toString = JavaKilg.toString;
   _.get = function(a) {
     return this.contents[a];
+  };
+  _.set = function(a, b) {
+    this.contents[a] = b;
+  };
+  _.call = function(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z) {
+    return this.contents[a](b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z);
+  };
+  _.2Dget = function(a, b) {
+    return this.contents[a][b];
   }
+  _.2Dset = function(a, b, c) {
+    this.contents[a][b] = c;
+  }
+  _.2Dcall = function(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, _a) {
+    return this.contents[a][b](c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, _a);
+  };
+  _.3Dget = function(a, b, c) {
+    return this.contents[a][b][c];
+  }
+  _.3Dset = function(a, b, c, d) {
+    this.contents[a][b][c] = d;
+  }
+  _.3Dcall = function(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, _a, _b) {
+    return this.contents[a][b][c](d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, _a, _b);
+  };
+  return _;
 };
+JavaKilg.2D = function(x, y) {
+  var _ = []
+  for(var i = 0; i < x; i++) {
+    _[i] = [];
+    for(var j = 0; j < y; j++) {
+      _[i][j] = undefined;
+    }
+  }
+  return _;
+}
+JavaKilg.3D = function(x, y, z) {
+  var _ = []
+  for(var i = 0; i < x; i++) {
+    _[i] = [];
+    for(var j = 0; j < y; j++) {
+      _[i][j] = [];
+      for(var k = 0; k < z; k++) {
+        _[i][j][k] = undefined;
+      }
+    }
+  }
+  return _;
+}
